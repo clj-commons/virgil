ccbef53df06e221e767b3abda339915f39fa2fff
Zach Tellman <ztellman@gmail.com>
2017-03-06 11:56:47 -0800

    mark 0.1.6

--- lein-virgil/src/lein_virgil/plugin.clj
+++ lein-virgil/src/lein_virgil/plugin.clj
@@ -1,11 +1,28 @@
 (ns lein-virgil.plugin
   (:require
-    [leiningen.core.eval :as eval]))
+   [leiningen.core.eval :as eval]))
+
+(def overwrites
+  '[[virgil "0.1.6"]
+    [org.ow2.asm/asm "5.1"]
+    [org.clojure/tools.namespace "0.2.11"]])
+
+(def injections
+  `((require 'virgil)
+    (virgil/watch ~@(:java-source-paths project))))
+
+(defn overwrite-dependencies [deps overwrites]
+  (let [project->dep (zipmap (map first deps) deps)]
+    (->> dependencies
+      (reduce
+        (fn [m [p v]] (assoc m p [p v]))
+        project->dep)
+      vals
+      vec)))
 
 (defn middleware [project]
   (if (contains? project :java-source-paths)
-    (let [dependencies '[[virgil "0.1.5"] [org.ow2.asm/asm "5.1"] [org.clojure/tools.namespace "0.2.11"]]
-          project' (-> project (update-in [:dependencies] into dependencies)
-                     (update-in [:injections] concat `((require 'virgil) (virgil/watch ~@(:java-source-paths project)))))]
-      project')
+    (-> project
+      (update-in [:dependencies] overwrite-dependencies overwrites)
+      (update-in [:injections] concat injections))
     project))
