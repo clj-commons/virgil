5e2d6d0cccabd7906f8078a47a47aa3d08339052
ztellman <ztellman@gmail.com>
2018-04-03 13:41:43 -0700

    mark 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject virgil "0.1.8-alpha1"
+(defproject virgil "0.1.8"
   :license {:name "MIT License"}
   :dependencies [[org.ow2.asm/asm "6.0"]
                  [org.clojure/tools.namespace "0.2.11"]]
