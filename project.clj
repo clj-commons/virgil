c7d289b85b1f788ddecfa6a7ac6639cd05c1d197
Emmanuel Oga <13634+EmmanuelOga@users.noreply.github.com>
2020-07-09 01:23:17 -0700

    Update ASM version to avoid errors with more modern JDKs
    ... otherwise it produces the following error:
    
    clojure unsupported class file major version 57
    
    ... when compiling under JDK 13 or later

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject virgil "0.1.9"
   :license {:name "MIT License"}
-  :dependencies [[org.ow2.asm/asm "7.0"]
+  :dependencies [[org.ow2.asm/asm "8.0.1"]
                  [org.clojure/tools.namespace "0.2.11"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
