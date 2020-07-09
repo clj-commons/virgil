d6df2197107ca184b9c90583b6022b61cdff3f7b
Emmanuel Oga <13634+EmmanuelOga@users.noreply.github.com>
2020-07-09 01:27:05 -0700

    Update version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject virgil "0.1.9"
+(defproject virgil "0.1.10"
   :license {:name "MIT License"}
   :dependencies [[org.ow2.asm/asm "8.0.1"]
                  [org.clojure/tools.namespace "0.2.11"]]
