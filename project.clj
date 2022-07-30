119f6512cf87b950936ea5d4a57f96866d8cb3b8
Alexander Yakushev <alex@bytopia.org>
2022-07-31 00:09:46 +0300

    Update ASM and tools.namespace dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject virgil "0.1.9"
+(defproject virgil "0.1.10-SNAPSHOT"
   :license {:name "MIT License"}
-  :dependencies [[org.ow2.asm/asm "7.0"]
-                 [org.clojure/tools.namespace "0.2.11"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
+  :dependencies [[org.ow2.asm/asm "9.3"]
+                 [org.clojure/tools.namespace "1.3.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}})
