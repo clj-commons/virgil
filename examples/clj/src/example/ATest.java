f2569f5fce83f5a2da191e20faa6e88ddc7a52a0
Alexander Yakushev <alex@bytopia.org>
2023-06-05 15:20:57 +0300

    Add usage examples

---
+++ examples/clj/src/example/ATest.java
@@ -0,0 +1,10 @@
+package example;
+
+public abstract class ATest {
+
+    public ATest() {}
+
+    public int magicNumber() {
+        return 42;
+    }
+}
---
+++ examples/lein/project.clj
@@ -0,0 +1,4 @@
+(defproject example "example"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[com.clojure-goes-fast/virgil "0.2.0-SNAPSHOT"]]}}
+  :main example)
