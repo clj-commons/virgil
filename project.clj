25c60bbbaee6cb8d494b5b0cd1823d2afaa37d90
Alexander Yakushev <alex@bytopia.org>
2023-06-05 14:24:37 +0300

    Add CI config

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject com.clojure-goes-fast/virgil "0.2.0-SNAPSHOT"
   :license {:name "MIT License"}
   :dependencies [[org.ow2.asm/asm "9.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}})
