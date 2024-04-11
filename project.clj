e42087f14b052f5e4963948e5ea86793c2ee4748
Alexander Yakushev <alex@bytopia.org>
2024-04-11 10:13:37 +0300

    Test against newer JDKs

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.clojure-goes-fast/virgil "0.2.0"
   :license {:name "MIT License"}
   :dependencies [[org.ow2.asm/asm "9.5"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}
-             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.2"]]}
+             :1.12 {:dependencies [[org.clojure/clojure "1.12.0-alpha9"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.2"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}})
