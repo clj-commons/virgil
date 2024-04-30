8c9e009ef6902899043933740100adb7edf17794
Oleksandr Yakushev <alex@bytopia.org>
2024-04-30 14:17:30 +0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject com.clojure-goes-fast/virgil "0.2.1"
   :license {:name "MIT License"}
   :dependencies [[org.ow2.asm/asm "9.7"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.2"]]}
-             :1.12 {:dependencies [[org.clojure/clojure "1.12.0-alpha9"]]}
-             :1.11 {:dependencies [[org.clojure/clojure "1.11.2"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.3"]]}
+             :1.12 {:dependencies [[org.clojure/clojure "1.12.0-alpha10"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}})
